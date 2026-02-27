24b385605f71a39a4b250281081b9e07a8b921b0
amidoumansa5-art <amidoumansa5@gmail.com>
2026-02-27 16:11:04 +0100

    Add Exercice02.java with personal introduction

---
+++ Exercice02.java
@@ -0,0 +1,12 @@
+public class Exercice2 {
+    public static void main(String[] args) {
+
+        String nom = "Amidou";
+        int age = 22;
+        double taille = 1.75;
+
+        System.out.println("Je m'appelle " + nom + 
+                           ", j'ai " + age + " ans, je mesure " 
+                           + taille + " m.");
+    }
+}
