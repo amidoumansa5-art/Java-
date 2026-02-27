0bbef0354278cf810148d3beaec5b4b92be1cb97
amidoumansa5-art <amidoumansa5@gmail.com>
2026-02-27 16:11:42 +0100

    Add Exercice3.java to calculate the sum of two numbers

---
+++ Exercice3.java
@@ -0,0 +1,11 @@
+public class Exercice3 {
+    public static void main(String[] args) {
+
+        int nombre1 = 10;
+        int nombre2 = 25;
+
+        int somme = nombre1 + nombre2;
+
+        System.out.println("La somme est : " + somme);
+    }
+}
